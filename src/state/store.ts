d2408bd7c7e50d5ae0da6885b89498c469cb9bc2
AnyaGalkina <anna.blackbird1@gmail.com>
2022-07-16 00:09:37 +0700

    Create store

---
+++ src/state/store.ts
@@ -0,0 +1,17 @@
+import {createStore, combineReducers} from "redux";
+import {TodolistsReducer} from "./todolists-reducer";
+import {TasksReducer} from "./tasks-reducer";
+
+
+export type AppRootState = ReturnType<typeof rootReducer>;
+
+const rootReducer = combineReducers({
+    todolists: TodolistsReducer,
+    tasks:  TasksReducer
+});
+
+export const store = createStore(rootReducer);
+
+
+//@ts-ignore
+window.store = store;
