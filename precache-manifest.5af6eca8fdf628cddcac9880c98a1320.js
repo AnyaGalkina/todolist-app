8a9951a5d4e37566c7dac1891bbd1dea3a7b39e8
AnyaGalkina <anna.blackbird1@gmail.com>
2022-07-30 00:32:37 +0700

    Updates

---
+++ precache-manifest.5af6eca8fdf628cddcac9880c98a1320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffab5d08823babbbf410526e0d5ad1d",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "a755205856977188dde0",
+    "url": "/todolist-app/static/css/main.ac3e58e6.chunk.css"
+  },
+  {
+    "revision": "ee94425008ced030144f",
+    "url": "/todolist-app/static/js/2.e69f8848.chunk.js"
+  },
+  {
+    "revision": "0fae7bcd2088ec9838aff549c47e18bb",
+    "url": "/todolist-app/static/js/2.e69f8848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a755205856977188dde0",
+    "url": "/todolist-app/static/js/main.7f3129a4.chunk.js"
+  },
+  {
+    "revision": "9cb8550a67995f624854",
+    "url": "/todolist-app/static/js/runtime-main.b742e4cb.js"
+  }
+]);
