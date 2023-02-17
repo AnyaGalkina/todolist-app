e02be28d1ad51fa81a6f50c4bfe3b62e7a0de85f
Anna Galkina <anna.blackbird1@gmail.com>
2023-02-17 14:28:21 +0700

    Delete icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,12 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import {useAppSelector} from "../../state/store";
-import {useAppDispatch} from "../../state/hooks";
-import {logout} from "../../features/Login/auth-reducer";
+import {useAppDispatch} from '../../state';
+import {logout, selectorIsLoggedIn} from '../../features';
+import {useSelector} from 'react-redux';
 
 export function Header() {
-    const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+    const isLoggedIn = useSelector(selectorIsLoggedIn);
     const dispatch = useAppDispatch();
 
 
@@ -26,17 +24,9 @@ export function Header() {
                     style={{backgroundColor: "#66b1d1"}}
             >
                 <Toolbar>
-                    <IconButton size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="menu"
-                                sx={{mr: 2}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
                     <Typography variant="h6"
                                 component="div"
-                                sx={{flexGrow: 1}}>Todolist
+                                sx={{flexGrow: 1, margin: '0 20px'}}>Todolist
                     </Typography>
                     {isLoggedIn && <Button color="inherit" onClick={onLoginClickHandler}>Logout</Button>}
                 </Toolbar>
